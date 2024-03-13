5cd01bd83fb51088ce6e5a7f45dfbfff9a9d5e54
Bianchi <9850842+athomasbianchi@users.noreply.github.com>
2024-03-12 21:36:48 -0400

    add position from fangraphs auction calculator

--- hitters.py
+++ hitters.py
@@ -2,11 +2,16 @@ import pandas as pd
 from os import path
 
 data = pd.read_csv('hitter.csv')
+pos = pd.read_csv('hitter_pos.csv')
 
-newdata = data.set_index('PlayerId')
+# newdata = data.set_index('PlayerId')
+newdata = data
 newdata['Points'] = newdata['1B'] + newdata['2B'] * 2 + newdata['3B'] * 3 + newdata['HR'] * 4 + newdata['R'] + newdata['RBI'] + newdata['BB'] + newdata['HBP'] + newdata['SB'] * 2
 newdata['PointAverage'] = newdata['Points'] / newdata['G']
 newdata.sort_values('Points', ascending=False, inplace=True)
 
-trimmed = newdata[['Name', 'Team', 'Points', 'PointAverage']]
-trimmed.to_csv('hitter_points.csv')
+trimmed = newdata[['Name', 'Team', 'Points', 'PointAverage', 'PlayerId']]
+
+combined = pd.merge(trimmed, pos[['POS', 'ADP', 'PlayerId']], on='PlayerId')
+print(combined.head())
+combined.to_csv('hitter_points.csv')
--- pitchers.py
+++ pitchers.py
@@ -2,11 +2,16 @@ import pandas as pd
 from os import path
 
 data = pd.read_csv('pitcher.csv')
+pos = pd.read_csv('pitcher_pos.csv')
 
-newdata = data.set_index('PlayerId')
+# newdata = data.set_index('PlayerId')
+newdata = data
 newdata['Points'] = newdata['IP'] * 2 - newdata['H'] / 2 - newdata['ER'] - newdata['BB'] / 2 - newdata['HBP'] / 2 + newdata['SO'] + newdata['W'] * 10 + newdata['SV'] * 5
 newdata['PointAverage'] = newdata['Points'] / newdata['G']
 newdata.sort_values('Points', ascending=False, inplace=True)
 
-trimmed = newdata[['Name', 'Team', 'Points', 'PointAverage']]
+trimmed = newdata[['Name', 'Team', 'Points', 'PointAverage', 'PlayerId']]
+
+combined = pd.merge(trimmed, pos[['POS', 'ADP', 'PlayerId']], on='PlayerId')
+print(combined.head())
 trimmed.to_csv('pitcher_points.csv')
