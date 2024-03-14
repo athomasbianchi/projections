bd86befeec603ef8f02090929a6ffa66c9397e5b
Bianchi <9850842+athomasbianchi@users.noreply.github.com>
2024-03-13 21:39:35 -0400

    use razzball for espn pos

--- hitters.py
+++ hitters.py
@@ -2,16 +2,18 @@ import pandas as pd
 from os import path
 
 data = pd.read_csv('hitter.csv')
-pos = pd.read_csv('hitter_pos.csv')
+pos = pd.read_csv('razzball.csv')
 
-# newdata = data.set_index('PlayerId')
-newdata = data
+newdata = data.rename(columns={'PlayerId': 'FangraphsId'})
+
 newdata['Points'] = newdata['1B'] + newdata['2B'] * 2 + newdata['3B'] * 3 + newdata['HR'] * 4 + newdata['R'] + newdata['RBI'] + newdata['BB'] + newdata['HBP'] + newdata['SB'] * 2
 newdata['PointAverage'] = newdata['Points'] / newdata['G']
 newdata.sort_values('Points', ascending=False, inplace=True)
 
-trimmed = newdata[['Name', 'Team', 'Points', 'PointAverage', 'PlayerId']]
+trimmed = newdata[['Name', 'Team', 'Points', 'PointAverage', 'FangraphsId']]
 
-combined = pd.merge(trimmed, pos[['POS', 'ADP', 'PlayerId']], on='PlayerId')
-print(combined.head())
+pos.rename(columns={'FangraphsID': 'FangraphsId', 'ESPN_POS': 'POS'}, inplace=True)
+
+combined = pd.merge(trimmed, pos[['POS', 'FangraphsId']], on='FangraphsId')
+
 combined.to_csv('hitter_points.csv')
--- pitchers.py
+++ pitchers.py
@@ -2,16 +2,18 @@ import pandas as pd
 from os import path
 
 data = pd.read_csv('pitcher.csv')
-pos = pd.read_csv('pitcher_pos.csv')
+pos = pd.read_csv('razzball.csv')
 
-# newdata = data.set_index('PlayerId')
-newdata = data
+newdata = data.rename(columns={'PlayerId': 'FangraphsId'})
+
 newdata['Points'] = newdata['IP'] * 2 - newdata['H'] / 2 - newdata['ER'] - newdata['BB'] / 2 - newdata['HBP'] / 2 + newdata['SO'] + newdata['W'] * 10 + newdata['SV'] * 5
 newdata['PointAverage'] = newdata['Points'] / newdata['G']
 newdata.sort_values('Points', ascending=False, inplace=True)
 
-trimmed = newdata[['Name', 'Team', 'Points', 'PointAverage', 'PlayerId']]
+trimmed = newdata[['Name', 'Team', 'Points', 'PointAverage', 'FangraphsId']]
 
-combined = pd.merge(trimmed, pos[['POS', 'ADP', 'PlayerId']], on='PlayerId')
-print(combined.head())
-trimmed.to_csv('pitcher_points.csv')
+pos.rename(columns={'FangraphsID': 'FangraphsId', 'ESPN_POS': 'POS'}, inplace=True)
+
+combined = pd.merge(trimmed, pos[['POS', 'FangraphsId']], on='FangraphsId')
+
+combined.to_csv('pitcher_points.csv')
